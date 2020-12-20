2cde92d0354b47606d1814b3526f1643b56c7973
abhishek <abhishek.pawar142@gmail.com>
2020-12-21 01:12:12 +0530

    visual

--- src/components/albumCard.jsx
+++ src/components/albumCard.jsx
@@ -11,11 +11,12 @@ export default function AlbumCard(props) {
         width: "18rem",
         margin: "1rem",
         borderRadius: "0.5rem",
+        background: "linear-gradient(rgba(255, 255, 255, 0.5), transparent)",
       }}
       className="align-self-start"
     >
       <Card.Img variant="top" src={imgurl} />
-      <Card.Body style={{ opacity: "75%" }}>
+      <Card.Body style={{ opacity: "65%" }}>
         <Card.Title>{name}</Card.Title>
         <Card.Subtitle>{date}</Card.Subtitle>
       </Card.Body>
