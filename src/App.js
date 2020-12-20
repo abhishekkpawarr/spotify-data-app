c7ebc1c727b829975e482fc863c9390e72f0b9b8
abhishek <abhishek.pawar142@gmail.com>
2020-12-21 00:16:31 +0530

    fixing cors error

--- src/App.js
+++ src/App.js
@@ -29,16 +29,12 @@ export default class App extends React.Component {
   }
 
   getAlbumData() {
-    fetch(
-      `https://cors-anywhere.herokuapp.com/https://api.spotify.com/v1/artists/${artistID}/albums`,
-      {
-        method: "get",
-        // mode: "cors",
-        headers: new Headers({
-          Authorization: `Bearer ${this.state.access_token}`,
-        }),
-      }
-    )
+    fetch(`https://api.spotify.com/v1/artists/${artistID}/albums`, {
+      method: "get",
+      headers: new Headers({
+        Authorization: `Bearer ${this.state.access_token}`,
+      }),
+    })
       .then((res) => {
         if (res.ok) {
           return res.json();
@@ -65,18 +61,14 @@ export default class App extends React.Component {
     }
     formBody = formBody.join("&");
 
-    fetch(
-      "https://cors-anywhere.herokuapp.com/https://accounts.spotify.com/api/token",
-      {
-        method: "post",
-        // mode: "cors",
-        headers: new Headers({
-          Authorization: `Basic ${encodedID}`,
-          "Content-Type": "application/x-www-form-urlencoded",
-        }),
-        body: formBody,
-      }
-    )
+    fetch("https://accounts.spotify.com/api/token", {
+      method: "post",
+      headers: new Headers({
+        Authorization: `Basic ${encodedID}`,
+        "Content-Type": "application/x-www-form-urlencoded",
+      }),
+      body: formBody,
+    })
       .then((res) => res.json())
       .then((res) => this.setState({ access_token: res.access_token }));
   }
