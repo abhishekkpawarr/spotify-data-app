fe34ed9b8b9e99fa728bba4bd0ede470af7e61bb
abhishek <abhishek.pawar142@gmail.com>
2020-12-21 10:31:31 +0530

    final final commit

--- src/components/albumCard.jsx
+++ src/components/albumCard.jsx
@@ -5,13 +5,15 @@ export default function AlbumCard(props) {
   const { name, date, imgurl } = props;
   return (
     <Card
+      bg="dark"
+      text="light"
+      border="light"
       style={{
         width: "15rem",
         borderRadius: "0.3rem",
         margin: "1.5rem",
-        padding: "0.05rem",
+        padding: "0.1rem",
         alignSelf: "start",
-        textAlign: "start",
       }}
     >
       <Card.Img src={imgurl} />
